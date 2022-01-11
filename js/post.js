e831f5ba5c0dba91d2135150e90a8d0b7c32d536
w3bsite <61429140+w3bsite@users.noreply.github.com>
2022-01-11 20:25:25 +0330

    single post from static object with functional links in list page

--- js/post.js
+++ js/post.js
@@ -209,60 +209,32 @@ var postObjects = [
 
 document.querySelector("#app").innerHTML = "";
 // const url = "https://jsonplaceholder.typicode.com";
-const row = document.querySelector("#row");
-var postList = postObjects;
-if (row) row.innerHTML = `<h2>loading</h2>`;
-var card = (title, content, article) => {
-  return `        <div class="card col-md-3">
+const container = document.querySelector("#article");
+// var postList = postObj;
+
+var card = (title, content) => {
+  return `        <div class="card col-md-12 mx-auto mt-5">
   <div class="card-body">
-    <h5 class="card-title"> ${title.substring(0, 20)}</h5>
- 
+    <h5 class="card-title"> ${title}</h5>
+
     <p class="card-text">
- ${content.substring(0, 70)}
+ ${content}
     </p>
-    <a href="/post.html?article=${article}" class="btn btnCard">Go somewhere</a>
+
   </div>
 </div>`;
 };
 
-const loadmore = document.querySelector("#loadmore");
-loadmore.innerHTML = `<div class="row justify-content-center" id='loadmore'>
-<button onclick="load(4)&buttom()" id="butt" type="button" class="btn color">
-<i class="bi bi-caret-down"></i>Load More</button>
-</div>`;
-
-function buttom() {
-  setTimeout(() => {
-    window.scroll({
-      top: 3000,
-      behavior: "smooth"
-    });
-  }, 500);
-}
-
-let i = 4;
-// function load(x) {
-//   row.innerHTML = ``;
-//   x ? (i = i + x) : (i = i + 0);
-//   fetch(url + "/posts")
-//     .then((response) => response.json())
-//     .then((res) => {
-//       res.slice(0, i).forEach((element) => {
-//         row.innerHTML += card(element.title, element.body);
-//       });
-//     });
-// }
-
 function load(x) {
-  row.innerHTML = ``;
-  x ? (i = i + x) : (i = i + 0);
-  postList.slice(0, i).forEach((element, i) => {
-    row.innerHTML += card(element.title, element.content, i);
-  });
+  container.innerHTML = ``;
+  var url = new URL(window.location);
+  var articleNumber = url.searchParams.get("article");
+  var article = postObjects[articleNumber];
+
+  container.innerHTML = card(article.title, article.content);
 }
 
 (async function () {
   await load(0);
 })();
 window.load = load;
-window.buttom = buttom;
---
+++ js/postObjects.js
@@ -0,0 +1,208 @@
+// var postObjects = [
+//   {
+//     id: 78,
+//     title: "My Brother Can Eat",
+//     conteent: null,
+//     date: null,
+//     boolian: null,
+//     obj: null,
+//     caption: null,
+//     published_at: "2021-01-22T20:41:38.412Z",
+//     created_at: "2021-01-22T20:41:38.429Z",
+//     updated_at: "2021-01-22T20:41:38.429Z",
+//     content:
+//       "My brother can eat.\nHide your fruits,\nHide your snacks,\nHide all your treats.\n\nIt doesn't matter what it is.\nY'all don't understand.\nThe boy can eat.\nCall him munchie man.\n\nRice and chicken,\nMac and cheese.\nHe's always in the kitchen\nSaying, \"More, please.\"\n\nJust the other day,\nHe ate all Daddy's steak.\nJust the other day,\nHe had three plates.\n\nI guarantee if you see him, he will ask,\n\"What are we going to eat?\"\nI love my brother lots,\nBut the boy sure can eat.\n\n ",
+//     bool: true
+//   },
+//   {
+//     id: 79,
+//     title: "The Thing!",
+//     conteent: null,
+//     date: null,
+//     boolian: null,
+//     obj: null,
+//     caption: null,
+//     published_at: "2021-01-22T20:42:12.662Z",
+//     created_at: "2021-01-22T20:42:12.673Z",
+//     updated_at: "2021-01-22T20:42:12.673Z",
+//     content:
+//       "I once set out to make a fiend,\nA gruesome thing that no one's seen.\nA massive gore of teeth and claws\nWith sixteen eyes and monster paws.\n\nBefore I started out to cook,\nI checked out first my Potions Book.\nWith some touches of my own,\nI knew I'd hear my monster moan.\n\nI finally found the perfect pot,\nHuge and black and rusted not,\nTo place it on a fire bright\nAnd waited for the dark of night.\n\nThe darkness came, none too soon,\nAnd in the shining of the moon,\nI began my task with hopes to fix\nThe makings for my monster mix.\n\nI filled the pot half-full with grease,\nAnd so began my monster-piece.\nI added pints of castor oil\nAnd waited for my mix to boil.\n\nI put in five potato eyes,\nA chicken's tooth and watched it rise.\nA lock of hair, a roach, a worm!\nI stirred it so it would not burn!\n\nBeetle guts, hornet's feet,\nFinger nails, rancid meat,\nA turtle's toes, a garter snake!\nSo far, this was a piece of cake!\n\nThen I put in weasel liver,\nA centipede, a caterpillar,\nSeven hairs from top a fly,\nA horse's tongue, a lizard's eye!\n\nOxen hooves and turkey neck,\nA gremlin's ear (just a speck),\nMaggot slime, a walrus knuckle.\nIt smelled so good, I had to chuckle!\n\nI listened to it curl and tip.\nI took a spoon and had a sip!\nAnd as I heard it bump and crunch,\nI settled back to have my lunch.\n\nI put the lid on good and tight.\nI let it simmer all that night.\nAnd all that day I let it brew.\nAll next night and the next day, too.\n\nThen, that night, the moment came\nTo see if I had worked in vain.\nI pried the lid with tender care,\nWondering what I'd find in there.\n\nI held my breath, peeked inside.\nI saw it...and I burst with pride!\nI threw the lid aside, at last,\nAnd danced around the pot of gas!\n\nAnd through the gas the creature rose,\nWith sixteen eyes, a big black nose,\nFangs, hair and ten inch claws,\nWarts, lice and monster paws!\n\nIt groaned! It gasped! It grunted, too!\nJust why it lived, I wish I knew!\n\"It must be luck! A chance! A fluke!\"\nI chose a name and called it, \"Duke\"!\n\n\"Here, Duke!\" I called, with outstretched hand.\nDid it really understand?\nIt did! It did! For Duke complied!\nIt looked so good, I almost cried!\n\nWith happy eyes I watched it slip,\nSquirm, slide, slither and dip.\nSlow, at first, then like a shot,\nDuke turned around....and ate the pot!\n\nIt ate the table and the chair,\nAnd my book a laying there!\nThen it ate my frying pans,\nMy overcoat and garbage cans.\n\nMy couch, my lamps, my stove, my suit!\nI have to say, it did look cute.\nBut was this the monster in my mind?\nDid I create a Frankenstein?!\n\nMy telephone and TV set,\nBut Duke? It wasn't finished yet!\nMy shower, toilet, tub and sink!\nMy sanity was at the brink!\n\nThen Duke stopped, to my surprise,\nAnd looked at me with thankful eyes.\nI never dreamed it'd be so wild!\nDuke cocked its head....and then it smiled.\n\nToday, it's eaten all I own!\nI barely even have a home!\nSo, I'm worried by one thing, you see,\nIt's that one day ... Duke ... might ... eat...\n...\"AARRGGHH!\"\n\n ",
+//     bool: true
+//   },
+//   {
+//     id: 83,
+//     title: "“The Stone Boy” by Gina Berriault (1957)",
+//     conteent: null,
+//     date: null,
+//     boolian: null,
+//     obj: null,
+//     caption: null,
+//     published_at: "2021-01-23T19:36:15.317Z",
+//     created_at: "2021-01-23T19:36:15.325Z",
+//     updated_at: "2021-01-23T19:36:15.325Z",
+//     content:
+//       "This great and underrated masterpiece is a meditation on good and evil and especially about the way that people’s expectations and assumptions about us may wear us down and eventually force us into compliance with their view. But it is a much deeper and more biblical story than that and, like any great work of art, resists reduction. Berriault, who died in 1999, is known as a San Francisco writer. A wonderful sampling of her stories is available in Women in Their Beds: New & Selected Stories. George Saunders",
+//     bool: true
+//   },
+//   {
+//     id: 84,
+//     title: "“The Love of a Good Woman” by Alice Munro (1998)",
+//     conteent: null,
+//     date: null,
+//     boolian: null,
+//     obj: null,
+//     caption: null,
+//     published_at: "2021-01-23T19:36:58.362Z",
+//     created_at: "2021-01-23T19:36:58.370Z",
+//     updated_at: "2021-01-23T19:36:58.370Z",
+//     content:
+//       "Among the handful of short stories closest to my heart, I’ve chosen “The Love of a Good Woman” by Canadian writer Munro, from her 1998 collection of that name. It’s about a murder – probably it’s a murder, because nothing is certain – and a love match that depends on keeping that murder secret. Like so many of Munro’s stories, this one has the scope of a novel yet never feels hurried or crowded. The sociology of a small town in rural Ontario is caught on the wing in the loose weave of her narration; the story takes in whole lifetimes, and yet its pace is also exquisitely slow, carrying us deep inside particular moments. A woman moves among the willows beside a river at night, making up her mind. Tessa Hadley",
+//     bool: true
+//   },
+//   {
+//     id: 85,
+//     title: "“The Love of a Good Woman” by Alice Munro (1998) copy",
+//     conteent: null,
+//     date: null,
+//     boolian: null,
+//     obj: null,
+//     caption: null,
+//     published_at: "2021-01-23T19:37:06.351Z",
+//     created_at: "2021-01-23T19:37:06.356Z",
+//     updated_at: "2021-01-23T19:37:06.356Z",
+//     content:
+//       "Among the handful of short stories closest to my heart, I’ve chosen “The Love of a Good Woman” by Canadian writer Munro, from her 1998 collection of that name. It’s about a murder – probably it’s a murder, because nothing is certain – and a love match that depends on keeping that murder secret. Like so many of Munro’s stories, this one has the scope of a novel yet never feels hurried or crowded. The sociology of a small town in rural Ontario is caught on the wing in the loose weave of her narration; the story takes in whole lifetimes, and yet its pace is also exquisitely slow, carrying us deep inside particular moments. A woman moves among the willows beside a river at night, making up her mind. Tessa Hadley",
+//     bool: true
+//   },
+//   {
+//     id: 86,
+//     title: "The Bat And The Weasels",
+//     conteent: null,
+//     date: null,
+//     boolian: null,
+//     obj: null,
+//     caption: null,
+//     published_at: "2021-01-23T19:42:21.642Z",
+//     created_at: "2021-01-23T19:42:21.646Z",
+//     updated_at: "2021-01-23T19:42:21.646Z",
+//     content:
+//       "A BAT who fell upon the ground and was caught by a Weasel pleaded to be spared his life. The Weasel refused, saying that he was by nature the enemy of all birds. The Bat assured him that he was not a bird, but a mouse, and thus was set free. Shortly afterwards the Bat again fell to the ground and was caught by another Weasel, whom he likewise entreated not to eat him. The Weasel said that he had a special hostility to mice. The Bat assured him that he was not a mouse, but a bat, and thus a second time escaped.",
+//     bool: true
+//   },
+//   {
+//     id: 87,
+//     title: "Hercules and the Wagoner Fable",
+//     conteent: null,
+//     date: null,
+//     boolian: null,
+//     obj: null,
+//     caption: null,
+//     published_at: "2021-01-23T20:35:05.618Z",
+//     created_at: "2021-01-23T20:35:05.643Z",
+//     updated_at: "2021-01-23T20:35:05.643Z",
+//     content:
+//       '\n\nA CARTER was driving a wagon along a country lane, when the wheels sank down deep into a rut. The rustic driver, stupefied and aghast, stood looking at the wagon, and did nothing but utter loud cries to Hercules to come and help him. Hercules, it is said, appeared and thus addressed him: "Put your shoulders to the wheels, my man. Goad on your bullocks, and never more pray to me for help, until you have done your best to help yourself, or depend upon it you will henceforth pray in vain."',
+//     bool: true
+//   },
+//   {
+//     id: 88,
+//     title: "hello sam fisher",
+//     conteent: null,
+//     date: null,
+//     boolian: null,
+//     obj: null,
+//     caption: null,
+//     published_at: "2021-02-01T13:29:36.376Z",
+//     created_at: "2021-02-01T13:29:36.404Z",
+//     updated_at: "2021-02-01T13:29:36.404Z",
+//     content:
+//       "Sometimes I can't find the words\nThat fill my messy head.\nCan't find the effort to smile\nOr get out of my silly old bed.\n",
+//     bool: true
+//   },
+//   {
+//     id: 91,
+//     title: "a new title for a post with an image to have in it",
+//     conteent: null,
+//     date: null,
+//     boolian: null,
+//     obj: null,
+//     caption: null,
+//     published_at: "2021-02-01T19:52:03.146Z",
+//     created_at: "2021-02-01T19:52:03.171Z",
+//     updated_at: "2021-02-01T19:52:03.171Z",
+//     content:
+//       "a new content for a post with an image to have in it , somthing like this:\nA brand new sand box survival game staged on diffrent imaginary planets and with base bulding ellements",
+//     bool: true
+//   },
+//   {
+//     id: 93,
+//     title: "a new title for a post with an image to have in it",
+//     conteent: null,
+//     date: null,
+//     boolian: null,
+//     obj: null,
+//     caption: "https://i.postimg.cc/qMVvdmX3/2.jpg",
+//     published_at: "2021-02-01T19:59:53.883Z",
+//     created_at: "2021-02-01T19:59:53.898Z",
+//     updated_at: "2021-02-08T14:39:30.132Z",
+//     content:
+//       "a new content for a post with an image to have in it , somthing like this:\nA **brand** new sand box survival game staged on diffrent imaginary planets and with base bulding ellements",
+//     bool: true
+//   },
+//   {
+//     id: 104,
+//     title: null,
+//     conteent: null,
+//     date: null,
+//     boolian: null,
+//     obj: null,
+//     caption: null,
+//     published_at: "2021-11-01T12:37:05.668Z",
+//     created_at: "2021-11-01T12:37:05.672Z",
+//     updated_at: "2021-11-01T12:37:05.672Z",
+//     content: null,
+//     bool: null
+//   },
+//   {
+//     id: 105,
+//     title: null,
+//     conteent: null,
+//     date: null,
+//     boolian: null,
+//     obj: null,
+//     caption: null,
+//     published_at: "2021-11-01T12:37:34.536Z",
+//     created_at: "2021-11-01T12:37:34.541Z",
+//     updated_at: "2021-11-01T12:37:34.541Z",
+//     content: null,
+//     bool: null
+//   },
+//   {
+//     id: 106,
+//     title: "saddd",
+//     conteent: null,
+//     date: null,
+//     boolian: null,
+//     obj: null,
+//     caption: null,
+//     published_at: "2021-11-01T12:39:49.625Z",
+//     created_at: "2021-11-01T12:39:49.627Z",
+//     updated_at: "2021-11-01T12:39:49.627Z",
+//     content: "saddddd",
+//     bool: null
+//   },
+//   {
+//     id: 107,
+//     title: "nuxt",
+//     conteent: null,
+//     date: null,
+//     boolian: null,
+//     obj: null,
+//     caption: null,
+//     published_at: "2021-11-01T12:40:05.673Z",
+//     created_at: "2021-11-01T12:40:05.675Z",
+//     updated_at: "2021-11-01T12:40:05.675Z",
+//     content: "nuxt",
+//     bool: null
+//   }
+// ];
